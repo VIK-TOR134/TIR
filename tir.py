f527416742e07336b58533711ff98a104e037859
VIK-TOR134 <vv34reg@yandex.ru>
2024-07-21 14:32:25 +0900

    deleted file image

--- tir.py
+++ tir.py
@@ -14,16 +14,16 @@ SCREEN_HEIGHT = 600  #5 шаг высота экрана
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # 6 шаг создание экрана
 
 pygame.display.set_caption("My Game") #7 шаг заголовок окна
-icon = pygame.image.load("icon.png") #8 шаг загрузка иконки
+icon = pygame.image.load("img/icon.png") #8 шаг загрузка иконки
 pygame.display.set_icon(icon) #9 шаг установка иконки
 
-target_image = pygame.image.load("target.png") #10 шаг создание переменной картинки цели
+target_image = pygame.image.load("img/target.png") #10 шаг создание переменной картинки цели
 target_width, target_height = 50 #11 шаг размеры ширины и высоты цели
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width) #12 шаг позиция цели по горизонтали (ось x)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height) #13 шаг позиция цели по вертикали (ось y)
 
-color = rondom.randint(0, 255), random.randint(0, 225), random.randint(0, 255) #14 шаг цвет цели выходит так же рондомно
+color = random.randint(0, 255), random.randint(0, 225), random.randint(0, 255) #14 шаг цвет цели выходит так же рондомно
 
 #цикл, в котором происходит основная игровая логика
 while running:
